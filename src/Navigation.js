ff07ca290f7173337b6820adf5a1e556de45c544
marija <mkantolic@unipu.hr>
2020-12-23 20:02:01 +0100

    update

---
+++ src/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div className="Navigation">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
